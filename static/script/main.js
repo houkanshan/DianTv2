7ccccc5f9cae8b8e7e04efa1eb667b2863b5ada9
houkanshan <houkanshan@gmail.com>
2012-09-15 04:45:12 +0800

    sass done, basic theme done

--- static/script/main.js
+++ static/script/main.js
@@ -1,27 +1,32 @@
 // Global require config
 require.config({
-    baseUrl: './',
-    paths: {
-        // here define the module name of jquery, 
-        // it couldn't be any other name since jquery define it in Line 9298
-        jquery: 'lib/jquery',
-        spine: 'lib/spine',
-        app: 'application'
-    },
-    shim: {
-        // spine doesn't support AMD, so define it here to let Global var
-        // to be the exports 
-        spine: {
-            deps: ['jquery'],
-            exports: 'Spine'
-        }
+  baseUrl: './',
+  paths: {
+    // here define the module name of jquery, 
+    // it couldn't be any other name since jquery define it in Line 9298
+    jquery: 'lib/jquery',
+    spine: 'lib/spine',
+    app: 'application'
+  },
+  shim: {
+    // spine doesn't support AMD, so define it here to let Global var
+    // to be the exports 
+    spine: {
+      deps: ['jquery'],
+      exports: 'Spine'
     }
+  }
 });
 
+var debugOn = true;
 
 // run application
-require(['jquery'], function($){
-    require(['application/diantv'], function(DianTv){
-        DianTv.init();
-    });
+require(['jquery'], function($) {
+  if (debugOn) {
+    require(['lib/live.js']);
+  }
+  require(['application/diantv'], function(DianTv) {
+    DianTv.init();
+  });
 });
+
